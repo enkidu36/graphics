b929297d382d83de3ea91a977c6e6453f3e56cbb
Joe Savoie <enkidu36@yahoo.com>
2023-04-15 15:09:21 -0500

    remove js directory from github

--- public/js/cljs-runtime/pbranes.app.web.js
+++
@@ -1,6 +0,0 @@
-goog.provide('pbranes.app.web');
-pbranes.app.web.init_BANG_ = (function pbranes$app$web$init_BANG_(){
-return cljs.core.println.cljs$core$IFn$_invoke$arity$variadic(cljs.core.prim_seq.cljs$core$IFn$_invoke$arity$2(["Hello Dolly!!"], 0));
-});
-
-//# sourceMappingURL=pbranes.app.web.js.map
--- public/js/cljs-runtime/shadow.module.main.append.js
+++
@@ -1,4 +0,0 @@
-
-shadow.cljs.devtools.client.env.module_loaded('main');
-
-try { pbranes.app.web.init_BANG_(); } catch (e) { console.error("An error occurred when calling (pbranes.app.web/init!)"); throw(e); }
