04816b392ba9986c9ce047743361337d4b965cd7
Joe Savoie <enkidu36@yahoo.com>
2023-04-16 17:33:55 -0500

    made canvas component

--- src/main/pbranes/page/book/ch01.cljs
+++ src/main/pbranes/page/book/ch01.cljs
@@ -2,25 +2,11 @@
   (:require [helix.core :refer [defnc <> $] ]
             [helix.dom :as d]
             [helix.hooks :as hooks]
-            [pbranes.webgl.utils :as u]))
+            [pbranes.webgl.utils :as u]
+            [pbranes.component.webgl-canvas :as canvas]))
 
 (defn main [gl]
    (u/update-clear-color gl [1.0 1.0 1.0 1.0]))
 
-(defnc page []
-  (let [glcanvas (hooks/use-ref nil)
-        paragraph (hooks/use-ref nil)]
-
-    (hooks/use-effect [] :once
-                      (let [ctx (u/get-webgl-context (.-current glcanvas))]
-                        (set! (.. paragraph -current -textContent) (u/webgl-support-msg ctx))
-                        (main ctx)
-                        (js/window.addEventListener "keydown" (u/check-key ctx))
-                        (fn unmount []
-                          (js/window.removeEventListener "keydown" (fn [e] (js/console.log "remove keydown listene"))))))
-
-       
-    (<>
-     (d/div {:class "webgl-support-msg"}
-            (d/p {:ref paragraph} "[ Here would go the results of WebGL feature detection ]"))
-     (d/canvas {:ref glcanvas :class "glcanvas"} "Sorry! No HTML5 Canvas was found on this page"))))
+(defn page []
+  ($ canvas/ webgl-canvas-component main))
