623dd71e7848641192bc4aaa61139256fc1fc188
Joe Savoie <enkidu36@yahoo.com>
2023-04-16 16:49:29 -0500

    cleanup util.cljs file

--- src/main/pbranes/webgl/utils.cljs
+++ src/main/pbranes/webgl/utils.cljs
@@ -9,12 +9,11 @@
     "Sorry! WebGL2 is not available"))
 
 (defn update-clear-color [gl [r g b a]]
-   (.clearColor gl r g b a)
+  (.clearColor gl r g b a)
   (.clear gl (.-COLOR_BUFFER_BIT gl))
- (.viewport gl 0 0 0 0) )
+  (.viewport gl 0 0 0 0) )
 
 (defn check-key [gl]
-  (js/console.log (Math/random))
   (fn [event]
     (case (.-keyCode event)
       49 (update-clear-color gl [0.2 0.8 0.2 1.0])
