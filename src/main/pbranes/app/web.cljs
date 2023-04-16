e1d6a7e13cf5216ee339781e71bccb04e21fd321
Joe Savoie <enkidu36@yahoo.com>
2023-04-16 07:10:56 -0500

    initial web page with layout and router

--- src/main/pbranes/app/web.cljs
+++ src/main/pbranes/app/web.cljs
@@ -1,4 +1,29 @@
-(ns pbranes.app.web)
+(ns pbranes.app.web
+  (:require [goog.dom :as gdom]
+            [helix.core :refer [defnc $]]
+            [helix.dom :as d]
+            ["react-dom/client" :as rdom]
+            ["react-router-dom" :as rr]))
 
-(defn init! []
-  (println "Hello Dolly!!"))
+(defnc home []
+  (d/div "Hello Dolly!!"))
+
+(defnc layout []
+  (d/div {:class "wrapper"}
+         (d/header {:class "header"}
+                   (d/nav {:class "nav"}
+                          ($ rr/Link {:to "/"} "Home")))))
+
+(defnc router []
+  ($ rr/Routes
+     ($ rr/Route {:path "/" :element ($ layout)}
+        ($ rr/Route {:path "/" :element ($ home)}))))
+
+(defnc app []
+  ($ rr/BrowserRouter
+     ($ router)))
+
+(defonce root  (rdom/createRoot (gdom/getElement "root")))
+
+(defn ^:dev/after-load init! []
+  (.render root ($ app)))
