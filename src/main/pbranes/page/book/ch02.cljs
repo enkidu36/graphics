d5d5ae03374f8f80bd4f00e59e7f526ae0379250
Joe Savoie <enkidu36@yahoo.com>
2023-04-21 22:53:50 -0500

    Finished rendering first square ch02

--- src/main/pbranes/page/book/ch02.cljs
+++ src/main/pbranes/page/book/ch02.cljs
@@ -7,70 +7,114 @@
 (set! *warn-on-infer* false)
 
 (def vs-shader
-  "
-#version 300 es 
+  "#version 300 es
 precision mediump float;
 
-uniform mat4 uModelViewMatrix;
-unitorm mat4 uProjectionMatrix;
-
-in vec3 aVertexPosition;
+// Supplied vertex position attribute
+ in  vec3 aVertexPosition;
 
 void main(void) {
-gl_Position = uProjectionMatrix * uModelViewMatrix * vec4(aVertexPosition, 1.0);
+  // Set the position in the clipspace coordinates
+  gl_Position = vec4(aVertexPosition, 1.0);
 }
 ")
 
 (def fs-shader
-  "
-  #version 300 es 
+  "#version 300 es
   precision mediump float;
 
-  out vec4 fragColor;
+ // Color that is the result of this shader
+  out  vec4 fragColor;
 
   void main(void) {
-      fragColor = vec4(uModelColor, 1.0);
+      fragColor = vec4(1.0, 0.0, 0.0, 1.0);
   }
 ")
 
+(def square-vertex-buffer (atom nil))
+(def square-index-buffer  (atom nil))
+(def program (atom nil))
+(def indices [0 1 2 0 2 3])
+
+(defn get-shader [gl source type]
+  (let [shader (.createShader gl type)]
+
+    ;; Compile the shader using the supplied shader code
+    (.shaderSource gl shader source)
+    (.compileShader gl shader)
+
+    ;; Ensure the shader is valid
+    (if  (not (.getShaderParameter gl shader (.-COMPILE_STATUS gl)))
+      (js/console.log  (.getShaderInfoLog gl shader))
+      shader)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Create a program with the appropriate vertex and fragment shaders
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (defn init-program [gl]
-  (let [vertex-shader (.getShader gl vs-shader)
-        fragment-shader (.getShader gl fs-shader)
-        program (.createProgram gl)]
+  (let [vertex-shader (get-shader gl vs-shader (.-VERTEX_SHADER gl))
+        fragment-shader (get-shader gl fs-shader (.-FRAGMENT_SHADER gl))]
+
+    ;; Create program
+    (reset! program (.createProgram gl))
 
     ;; Attach the shaders to the program
-    (.attachShader gl program vertex-shader)
-    (.attachShader gl program fragment-shader)
-    (.linkProgram gl program)
+    (.attachShader gl @program vertex-shader)
+    (.attachShader gl @program fragment-shader)
+    (.linkProgram gl @program)
 
-    (if (not (.getProgramParameter gl program (.-LINK_STATUS gl)))
+    (when (not (.getProgramParameter gl @program (.-LINK_STATUS gl)))
       (js/console.error  "Could not initialize shaders."))
 
     ;; Use this program instance
-    (.useProgram gl program)
-    
-    ;; We attache the location of these shaders values to the program instance
+    (.useProgram gl @program)
+
+    ;; We attach the location of these shaders values to the program instance
     ;; for easy access later in the code
-    (swap! (.-aVertexPosition gl) (.getAttribLocation gl program "aVertexPosition"))))
+    (set! (.-aVertexPosition gl)  (.getAttribLocation gl @program "aVertexPosition"))))
 
 (defn init-buffers [gl]
-  (let [vertices  [-0.5 0.5 0         -0.5 -0.5 0         0.5 0.5 0]
-        indicies [0 1 2 0 2 3]
-        square-vertex-buffer (.createBuffer gl)
-        square-index-buffer (.createBuffer gl)]
+  (let [vertices  [-0.5 0.5 0        -0.5 -0.5 0         0.5 -0.5 0    0.5 0.5 0]]
 
-    ;; Setting up the VBO
-    (.bindBuffer gl (.-ARRAY_BUFFER gl) square-vertex-buffer)
-    (.bindBufferData gl (.-ARRAY_BUFFER gl) (js/Float32Array.  vertices) (.-STATIC_DRAW gl))
+;; Setting up the VBO
+    (reset! square-vertex-buffer (.createBuffer gl))
+    (.bindBuffer gl (.-ARRAY_BUFFER gl) @square-vertex-buffer)
+    (.bufferData gl (.-ARRAY_BUFFER gl) (js/Float32Array.  vertices) (.-STATIC_DRAW gl))
 
     ;; Setting up the IBO
-    (.bindBuffer gl  (.-ELEMENT_ARRAY_BUFFER gl) square-index-buffer)
-    (.bindData gl (.-ELEMENT_ARRAY_BUFFER gl) (js/Uint16Array.  indicies) (.-STATIC_DRAW gl))
+    (reset! square-index-buffer (.createBuffer gl))
+    (.bindBuffer gl  (.-ELEMENT_ARRAY_BUFFER gl) @square-index-buffer)
+    (.bufferData  gl (.-ELEMENT_ARRAY_BUFFER gl) (js/Uint16Array.  indices) (.-STATIC_DRAW gl))
 
     ;; Clean
     (.bindBuffer gl (.-ARRAY_BUFFER gl) nil)
-    (.bindBuffer.gl (.-ELEMENT_ARRAY_BUFFER gl) nil)
-    ))
+    (.bindBuffer gl (.-ELEMENT_ARRAY_BUFFER gl) nil)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; We call to render to our canvas
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defn draw [gl]
+  ;; Clear the screen
+  (.clear gl  (bit-or (.-COLOR_BUFFER_BIT gl) (.-DEPTH_BUFFER_BIT gl)))
+  (.viewport gl 0 0 (.. gl  -canvas -width) (.. gl -canvas -height))
+
+  ;; Use the buffers we've constructed
+  (.bindBuffer gl (.-ARRAY_BUFFER gl) @square-vertex-buffer)
+  (.vertexAttribPointer gl (.-aVertexPosition @program) 3 (.-FLOAT gl) false 0 0)
+  (.enableVertexAttribArray gl (.-aVertexPosition @program))
+
+  ;; Bind IBO
+  (.bindBuffer gl (.-ELEMENT_ARRAY_BUFFER gl) @square-index-buffer)
+
+  ;; Draw t the scened using triangle primitives
+  (.drawElements gl (.-TRIANGLES gl) (count indices) (.-UNSIGNED_SHORT gl) 0)
+
+  ;; Clean
+  (.bindBuffer gl (.-ARRAY_BUFFER gl) nil)
+  (.bindBuffer gl  (.-ELEMENT_ARRAY_BUFFER gl) nil)
+
+  ;;
+  )
 
 (defn main [canvas]
   (set! (.-width canvas) (- (.-clientWidth (.-parentNode canvas)) 60))
@@ -78,7 +122,12 @@ gl_Position = uProjectionMatrix * uModelViewMatrix * vec4(aVertexPosition, 1.0);
   (let [gl (u/get-webgl-context canvas)]
 
     (.clearColor gl 0 0 0 1)
-    (.enable gl (.-DEPTH-TEST  gl))))
+
+    ;; Call the functions in an appropriate order
+    (init-program gl)
+    (init-buffers gl)
+   (draw gl)
+    ))
 
 (defnc page []
   (let [canvas (hooks/use-ref nil)]
