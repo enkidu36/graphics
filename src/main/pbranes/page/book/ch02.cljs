8ca226105703eec341451fe51f915e35a3a2a856
Joe Savoie <enkidu36@yahoo.com>
2023-04-22 09:27:34 -0500

    square with VAO

--- src/main/pbranes/page/book/ch02.cljs
+++ src/main/pbranes/page/book/ch02.cljs
@@ -31,8 +31,8 @@ void main(void) {
   }
 ")
 
-(def square-vertex-buffer (atom nil))
-(def square-index-buffer  (atom nil))
+(def square-vao (atom nil))
+(def square-index-buffer (atom nil))
 (def program (atom nil))
 (def indices [0 1 2 0 2 3])
 
@@ -74,19 +74,32 @@ void main(void) {
     (set! (.-aVertexPosition gl)  (.getAttribLocation gl @program "aVertexPosition"))))
 
 (defn init-buffers [gl]
-  (let [vertices  [-0.5 0.5 0        -0.5 -0.5 0         0.5 -0.5 0    0.5 0.5 0]]
+  (let [vertices  [
+                   -0.5 0.5 0
+                   -0.5 -0.5 0
+                   0.5 -0.5 0
+                   0.5 0.5 0]]
 
 ;; Setting up the VBO
-    (reset! square-vertex-buffer (.createBuffer gl))
-    (.bindBuffer gl (.-ARRAY_BUFFER gl) @square-vertex-buffer)
+    (reset! square-vao (.createVertexArray gl))
+
+    (.bindVertexArray gl @square-vao)
+    
+    ;; Create/bind  buffer for vertex array
+    (.bindBuffer gl (.-ARRAY_BUFFER gl) (.createBuffer gl))
     (.bufferData gl (.-ARRAY_BUFFER gl) (js/Float32Array.  vertices) (.-STATIC_DRAW gl))
 
+    ;; Provide instructions for VAO to use later in Draw
+    (.enableVertexAttribArray gl (.-aVertexPosition @program))
+    (.vertexAttribPointer gl (.-aVertexPosition @program) 3 (.-FLOAT gl) false 0 0)
+    
     ;; Setting up the IBO
     (reset! square-index-buffer (.createBuffer gl))
     (.bindBuffer gl  (.-ELEMENT_ARRAY_BUFFER gl) @square-index-buffer)
     (.bufferData  gl (.-ELEMENT_ARRAY_BUFFER gl) (js/Uint16Array.  indices) (.-STATIC_DRAW gl))
 
-    ;; Clean
+       ;; Clean
+    (.bindVertexArray gl nil)
     (.bindBuffer gl (.-ARRAY_BUFFER gl) nil)
     (.bindBuffer gl (.-ELEMENT_ARRAY_BUFFER gl) nil)))
 
@@ -98,22 +111,15 @@ void main(void) {
   (.clear gl  (bit-or (.-COLOR_BUFFER_BIT gl) (.-DEPTH_BUFFER_BIT gl)))
   (.viewport gl 0 0 (.. gl  -canvas -width) (.. gl -canvas -height))
 
-  ;; Use the buffers we've constructed
-  (.bindBuffer gl (.-ARRAY_BUFFER gl) @square-vertex-buffer)
-  (.vertexAttribPointer gl (.-aVertexPosition @program) 3 (.-FLOAT gl) false 0 0)
-  (.enableVertexAttribArray gl (.-aVertexPosition @program))
-
-  ;; Bind IBO
-  (.bindBuffer gl (.-ELEMENT_ARRAY_BUFFER gl) @square-index-buffer)
-
-  ;; Draw t the scened using triangle primitives
+  ;; Bind the VAO
+  (.bindVertexArray gl @square-vao)
+  
+    ;; Draw t the scened using triangle primitives
   (.drawElements gl (.-TRIANGLES gl) (count indices) (.-UNSIGNED_SHORT gl) 0)
 
   ;; Clean
-  (.bindBuffer gl (.-ARRAY_BUFFER gl) nil)
-  (.bindBuffer gl  (.-ELEMENT_ARRAY_BUFFER gl) nil)
-
-  ;;
+  (.bindVertexArray gl nil)
+    ;;
   )
 
 (defn main [canvas]
@@ -133,8 +139,7 @@ void main(void) {
   (let [canvas (hooks/use-ref nil)]
 
     (hooks/use-effect []
-                      :once
-                      (set! (.-width canvas) (.-innerWidth js/window))
-                      (js/console.log "width" (.-width canvas))
-                      (main (.-current canvas)))
+      :once
+      (set! (.-width canvas) (.-innerWidth js/window))
+      (main (.-current canvas)))
     (d/canvas {:id "ch02-canvas" :ref canvas :class "canvas"})))
